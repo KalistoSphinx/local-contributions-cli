8733be8b66d3cdf5c2322676beb853b01e657ddc
KalistoSphinx <shouryadeshmukh6265@gmail.com>
2026-04-23 01:35:04 +0530

    added folder scanning

--- main.go
+++ main.go
@@ -3,61 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"os"
-	"os/user"
-	"strings"
 )
-
-func scanGitFolders(folders []string, folder string) []string {
-	folder = strings.TrimSuffix(folder, "/")
-	f, err := os.Open(folder)
-	if err != nil {
-		log.Fatal(err)
-	}
-	files, err := f.ReadDir(-1)
-	f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var path string
-	for _, file := range files {
-		if file.IsDir() {
-			path = folder + "/" + file.Name()
-			if file.Name() == ".git" {
-				path = strings.TrimSuffix(path, "/.git")
-				fmt.Println(path)
-				folders = append(folders, path)
-				continue
-			}
-			if file.Name() == "vendor" || file.Name() == "node_modules" {
-				continue
-			}
-
-			folders = scanGitFolders(folders, path)
-		}
-	}
-
-	return folders
-}
-
-func recursiveScanFolder(folder string) []string {
-	return scanGitFolders(make([]string, 0), folder)
-}
-
-func getDotFilePath() string {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dotFile := usr.HomeDir + "./gogitlocalstats"
-	return dotFile
-}
 
 func stats(email string) {
 	print("stats")
+
+}
+
+func scan(folder string) {
+	fmt.Printf("Found folders:\n\n")
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementToFile(filePath, repositories)
+	fmt.Printf("\n\nSuccessfully added\n\n")
 }
 
 func main() {
